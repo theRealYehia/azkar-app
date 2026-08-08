3d1eecd759b8a4ff07b726e0eb72bdef403484e4
theRealYehia <yehiaebrahim278@gmail.com>
2026-08-08 07:18:59 +0300

    Create AlarmReceiver.kt

---
+++ app/src/main/java/com/yehia/azkar/AlarmReceiver.kt
@@ -0,0 +1,12 @@
+package com.yehia.azkar
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        NotificationHelper.showAzkarNotification(context)
+        AlarmScheduler.scheduleNext(context)
+    }
+}

69286bad2eea5cf819734a689b9634c2e27a12c7
theRealYehia <yehiaebrahim278@gmail.com>
2026-08-08 07:20:17 +0300

    Create BootReceiver.kt

---
+++ app/src/main/java/com/yehia/azkar/BootReceiver.kt
@@ -0,0 +1,15 @@
+package com.yehia.azkar
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            if (AlarmScheduler.isEnabled(context)) {
+                AlarmScheduler.scheduleNext(context)
+            }
+        }
+    }
+}
